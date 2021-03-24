25a1dae50a57f37855639724a484d0e831b339bc
etff <wish2rich@gmail.com>
2021-03-24 20:14:55 +0900

    feature(study): add study domain

---
+++ src/main/java/com/mogaco/project/study/domain/Study.java
@@ -0,0 +1,80 @@
+package com.mogaco.project.study.domain;
+
+import com.mogaco.project.config.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Transient;
+import java.time.LocalDate;
+
+/**
+ * 스터디 정보.
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Study extends BaseEntity {
+
+    /**
+     * 스터디 식별자.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    /**
+     * 스터디 일자.
+     */
+    private LocalDate startedAt;
+
+    /**
+     * 스터디 시간.
+     */
+    private String time;
+
+    /**
+     * 참가인원.
+     */
+    @Transient
+    private int count;
+
+    /**
+     * 장소.
+     */
+    private String location;
+
+    /**
+     * 제목.
+     */
+    private String title;
+
+    /**
+     * 메시지.
+     */
+    @Lob
+    private String message;
+
+    /**
+     * 나의 주제.
+     */
+    private String myStudy;
+
+    @Builder
+    public Study(Long id, LocalDate startedAt, String time, int count, String location, String title, String message, String myStudy) {
+        this.id = id;
+        this.startedAt = startedAt;
+        this.time = time;
+        this.count = count;
+        this.location = location;
+        this.title = title;
+        this.message = message;
+        this.myStudy = myStudy;
+    }
+}
---
+++ src/test/java/com/mogaco/project/study/domain/StudyTest.java
@@ -0,0 +1,38 @@
+package com.mogaco.project.study.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("스터디 클래스")
+class StudyTest {
+    private static final String TIME = "14:00 ~ 16:00";
+    private static final String TITLE = "let's study";
+    private static final String LOCATION = "seoul hongdae";
+    private static final String MESSAGE = "share with me";
+    private static final String MY_STUDY = "SPRING";
+    private static final LocalDate STARTED_AT = LocalDate.of(2021, 4, 10);
+
+    @DisplayName("스터디를 생성하고, 생성자 값을 리턴받을 수 있다")
+    @Test
+    void create() {
+        Study study = Study.builder()
+                .startedAt(STARTED_AT)
+                .time(TIME)
+                .title(TITLE)
+                .location(LOCATION)
+                .message(MESSAGE)
+                .myStudy(MY_STUDY)
+                .build();
+
+        assertThat(study.getStartedAt()).isEqualTo(STARTED_AT);
+        assertThat(study.getTime()).isEqualTo(TIME);
+        assertThat(study.getTitle()).isEqualTo(TITLE);
+        assertThat(study.getLocation()).isEqualTo(LOCATION);
+        assertThat(study.getMessage()).isEqualTo(MESSAGE);
+        assertThat(study.getMyStudy()).isEqualTo(MY_STUDY);
+    }
+}
