082c8e41995f975adc1a2b62a173de638e88f464
etff <wish2rich@gmail.com>
2021-03-25 22:20:50 +0900

    refactor: change error message to final

--- src/main/java/com/mogaco/project/global/errors/ErrorResponse.java
+++ src/main/java/com/mogaco/project/global/errors/ErrorResponse.java
@@ -1,7 +1,7 @@
 package com.mogaco.project.global.errors;
 
 public class ErrorResponse {
-    private String message;
+    private final String message;
 
     public ErrorResponse(String message) {
         this.message = message;
