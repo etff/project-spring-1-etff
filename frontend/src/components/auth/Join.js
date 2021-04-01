92082a93cc9fc4c062e850a200f04446a3bddcf1
etff <wish2rich@gmail.com>
2021-04-01 20:41:46 +0900

    feature(register): add member register form

--- frontend/src/components/auth/Join.js
+++ frontend/src/components/auth/Join.js
@@ -1,5 +1,145 @@
+import "antd/dist/antd.css";
+import {Button, Col, Form, Input, Row, Typography,} from "antd";
+import {Content} from "antd/lib/layout/layout";
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+/* eslint-disable no-template-curly-in-string */
+
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not a valid email!",
+    password: "${label} is not a valid password!",
+  },
+  password: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
+
+const {Title} = Typography;
+
 const Join = () => {
-  return <div>Join</div>;
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  return (
+      <>
+        <Content>
+          <div style={{padding: 60, minHeight: "80vh"}}>
+            <Row>
+              <Col span={4} offset={10}>
+                <Title>회원가입</Title>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={8} push={6}>
+                <Form
+                    {...layout}
+                    name="register"
+                    onFinish={onFinish}
+                    validateMessages={validateMessages}
+                    scrollToFirstError
+                >
+                  <Form.Item
+                      name="email"
+                      label="E-mail"
+                      rules={[
+                        {
+                          type: "email",
+                          message: "유효한 이메일이 아닙니다!",
+                        },
+                        {
+                          required: true,
+                          message: "이메일을 입력해주세요!",
+                        },
+                      ]}
+                  >
+                    <Input/>
+                  </Form.Item>
+                  <Form.Item
+                      name="name"
+                      label="이름"
+                      rules={[
+                        {
+                          required: true,
+                          message: "이름을 입력해주세요ㅕ!",
+                        },
+                      ]}
+                  >
+                    <Input/>
+                  </Form.Item>
+
+                  <Form.Item
+                      name="password"
+                      label="비밀번호"
+                      rules={[
+                        {
+                          required: true,
+                          message: "비밀번호를 입력해주세요",
+                        },
+                        {
+                          min: 1,
+                          max: 24,
+                          message: "비밀번호가 유효하지 않습니다!",
+                        },
+                      ]}
+                      hasFeedback
+                  >
+                    <Input.Password/>
+                  </Form.Item>
+
+                  <Form.Item
+                      name="confirm"
+                      label="비밀번호 확인"
+                      dependencies={["password"]}
+                      hasFeedback
+                      rules={[
+                        {
+                          required: true,
+                          message: "비밀번호를 확인해주세요!",
+                        },
+                        ({getFieldValue}) => ({
+                          validator(_, value) {
+                            if (!value || getFieldValue("password") === value) {
+                              return Promise.resolve();
+                            }
+
+                            return Promise.reject(
+                                new Error(
+                                    "The two passwords that you entered do not match!"
+                                )
+                            );
+                          },
+                        }),
+                      ]}
+                  >
+                    <Input.Password/>
+                  </Form.Item>
+
+                  <Form.Item wrapperCol={{...layout.wrapperCol, offset: 8}}>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        className="login-form-button"
+                    >
+                      확인
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Col>
+            </Row>
+          </div>
+        </Content>
+      </>
+  );
 };
 
 export default Join;
--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,5 +1,87 @@
+import "antd/dist/antd.css";
+import {Button, Col, Form, Input, Row, Typography} from "antd";
+import {Helmet} from "react-helmet";
+import {Content} from "antd/lib/layout/layout";
+import {LockOutlined, UserOutlined} from "@ant-design/icons";
+
+const {Title} = Typography;
 const Login = () => {
-  return <div>Login</div>;
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+      <>
+        <Helmet title="Login"/>
+        <Content>
+          <div style={{padding: 100, minHeight: "80vh"}}>
+            <Row>
+              <Col span={8} offset={8}>
+                <Title>로그인</Title>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={8} offset={8}>
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                      remember: true,
+                    }}
+                    onFinish={onFinish}
+                >
+                  <Form.Item
+                      name="email"
+                      rules={[
+                        {
+                          required: true,
+                          type: "email",
+                          message: "Please input your Email!",
+                        },
+                      ]}
+                  >
+                    <Input
+                        prefix={<UserOutlined className="site-form-item-icon"/>}
+                        placeholder="Email"
+                    />
+                  </Form.Item>
+                  <Form.Item
+                      name="password"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your Password!",
+                        },
+                      ]}
+                  >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                        type="password"
+                        placeholder="Password"
+                    />
+                  </Form.Item>
+                  <Form.Item>
+                    <a className="login-form-forgot" href="">
+                      Forgot password
+                    </a>
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        className="login-form-button"
+                    >
+                      Log in
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Col>
+            </Row>
+          </div>
+        </Content>
+      </>
+  );
 };
 
 export default Login;
