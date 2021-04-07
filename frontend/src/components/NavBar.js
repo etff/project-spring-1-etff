c7e6a8906891d56a2735b27918f1c865dfa9ec04
etff <wish2rich@gmail.com>
2021-04-07 20:39:43 +0900

    feature(meet-detail): add meeting detail layout
    Develop (#4)
    
    * feature(login): add login access frontpage
    
    backend login 연동
    
    * fix(login): remove import warnings
    
    * feature(login): toggle login button
    
    Merge branch 'main' of https://github.com/etff/project-spring-1-etff into feature-load

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,14 +1,13 @@
 import {useCallback} from "react";
 import {Header} from "antd/lib/layout/layout";
-import {HomeTwoTone} from "@ant-design/icons";
+import {HomeTwoTone, UserOutlined} from "@ant-design/icons";
 import {useDispatch, useSelector} from "react-redux";
-import {Col, Menu, Row} from "antd";
+import {Avatar, Button, Col, Row, Space} from "antd";
 import {Link} from "react-router-dom";
 import {LOGOUT_REQUEST} from "../redux/types";
 
 const NavBar = () => {
   const dispatch = useDispatch();
-
   const {isAuthenticated} = useSelector((state) => state.auth);
 
   const onLogout = useCallback(() => {
@@ -29,23 +28,28 @@ const NavBar = () => {
             </div>
           </Col>
           <Col span={4}>
-            <Menu theme="dark" mode="horizontal">
+            <Space size="middle">
               {!isAuthenticated ? (
-                  <Menu.Item key="1">
-                    <Link to="/login">LOGIN</Link>
-                  </Menu.Item>
+                  <Link to="/login">
+                    <Button type="dashed">LOGIN</Button>
+                  </Link>
               ) : (
-                  <Menu.Item key="1">
-                    <Link onClick={onLogout} to="#">
-                      Logout
-                    </Link>
-                  </Menu.Item>
+                  <Link onClick={onLogout} to="#">
+                    <Button type="dashed">LOGOUT</Button>
+                  </Link>
               )}
-
-              <Menu.Item key="2">
-                <Link to="/join">Join</Link>
-              </Menu.Item>
-            </Menu>
+              <Link to="/join">
+                <Button type="dashed">JOIN</Button>
+              </Link>
+              <Link>
+                <Avatar
+                    style={{
+                      backgroundColor: "#87d068",
+                    }}
+                    icon={<UserOutlined/>}
+                />
+              </Link>
+            </Space>
           </Col>
         </Row>
       </Header>
