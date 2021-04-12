ba2e63e23bb9ea41a477a4eb1f91dc92decf4ddb
etff <wish2rich@gmail.com>
2021-04-12 12:45:08 +0900

    refactor(member): change variable name userId to memberId

--- src/main/java/com/mogaco/project/member/infra/JpaRoleRepository.java
+++ src/main/java/com/mogaco/project/member/infra/JpaRoleRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface JpaRoleRepository extends RoleRepository, CrudRepository<Role, Long> {
-    List<Role> findAllByUserId(Long userId);
+    List<Role> findAllByMemberId(Long memberId);
 
     Role save(Role role);
 }
