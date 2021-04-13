0a31c5ee1fb9c340a394f192420f03b6f50f3d63
etff <wish2rich@gmail.com>
2021-04-13 18:40:15 +0900

    fix(layout): change navigation path

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -2,32 +2,32 @@ import {Layout} from "antd";
 import Main from "../components/meet/Main";
 import Login from "../components/auth/Login";
 import Join from "../components/auth/Join";
-import NavBar from "../components/navigation/NavBar";
 import {Redirect, Route, Switch} from "react-router-dom";
 import {Content} from "antd/lib/layout/layout";
 import Foot from "../components/Foot";
 import MeetCreate from "../components/meet/MeetCreate";
 import MeetDetail from "../components/meet/MeetDetail";
+import NavBar from "../components/navigation";
 
 const MyRouter = () => {
   return (
       <>
-          <NavBar/>
-          <Layout>
-              <Layout className="site-layout-background">
-                  <Content style={{padding: "0 50px"}}>
-                      <Switch>
-                          <Route path="/" exact component={Main}/>
-                          <Route path="/login" exact component={Login}/>
-                          <Route path="/join" exact component={Join}/>
-                          <Route path="/meet/:id" exact component={MeetDetail}/>
-                          <Route path="/meet-create" exact component={MeetCreate}/>
-                          <Redirect from="*" to="/"/>
-                      </Switch>
-                  </Content>
-                  <Foot/>
-              </Layout>
+        <NavBar/>
+        <Layout>
+          <Layout className="site-layout-background">
+            <Content style={{padding: "0 50px"}}>
+              <Switch>
+                <Route path="/" exact component={Main}/>
+                <Route path="/login" exact component={Login}/>
+                <Route path="/join" exact component={Join}/>
+                <Route path="/meet/:id" exact component={MeetDetail}/>
+                <Route path="/meet-create" exact component={MeetCreate}/>
+                <Redirect from="*" to="/"/>
+              </Switch>
+            </Content>
+            <Foot/>
           </Layout>
+        </Layout>
       </>
   );
 };
