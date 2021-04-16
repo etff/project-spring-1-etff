639ed7e3f01718096736804fad01a33f999d3667
etff <wish2rich@gmail.com>
2021-04-16 17:58:10 +0900

    fix: deploy ssl

--- src/main/java/com/mogaco/project/global/config/WebConfig.java
+++ src/main/java/com/mogaco/project/global/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/api/**")
+                .allowedHeaders("*")
                 .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("https://www.mogaco.net")
                 .allowedOrigins("http://www.mogaco.net");
